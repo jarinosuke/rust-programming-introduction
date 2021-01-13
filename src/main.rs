a013f7630da2ea03e08f82170533efba530b3b62
jarinosuke(Naoki ISHIKAWA) <naokiishikawa0808@gmail.com>
2021-01-14 08:03:35 +0900

    done concurrent section (Future)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use std::{vec, writeln};
+//use std::{fmt::rt::v1::Count, vec, writeln};
 use std::io::Write;
+use futures::{executor, future::join_all};
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use async_trait::async_trait;
 
 fn main() {
     println!("Hello, world!");
@@ -170,6 +175,17 @@ fn main() {
     write!(&mut w, "{}", "ABC");
     writeln!(&mut w, " is 123");
     dbg!(w);
+
+    //Future
+    let countdown_future1 = CountDown(10);
+    let countdown_future2 = CountDown(20);
+    let cd_set = join_all(vec![countdown_future1, countdown_future2]);
+    let res = executor::block_on(cd_set);
+    for (i, s) in res.iter().enumerate() {
+        println!("{}: {}", i, s);
+    }
+
+    executor::block_on(something_great_async_function());
 }
 
 fn func(code: i32) -> Result<i32, String> {
@@ -237,5 +253,57 @@ impl Iterator for Iter {
         } else {
             None
         }
+    }
+}
+// Future
+struct CountDown(u32);
+
+impl Future for CountDown {
+    type Output = String;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.0 == 0 {
+            Poll::Ready("Zero!!!".to_string())
+        } else {
+            println!("{}", self.0);
+            self.0 -= 1;
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
+
+async fn async_add(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+async fn something_great_async_function() -> i32 {
+    let ans1 = async_add(2, 3).await;
+    let ans2 = async_add(3, 4).await;
+    let ans3 = async_add(4, 5).await;
+    let result = ans1 + ans2 + ans3;
+    println!("{}", result);
+    result 
+}
+
+fn something_great_async_function2() -> impl Future<Output = i32> {
+    async {
+        let ans = async_add(3, 2).await;
+        println!("{}", ans);
+        ans
+    }
+}
+
+fn move_to_async_block() -> impl Future<Output = ()> {
+    let outside_variable = "this is outside".to_string();
+    async move {
+        println!("{}", outside_variable);
+    }
+}
+
+#[async_trait]
+trait AsyncTrait {
+    async fn f() {
+        println!("test");
     }
 }
