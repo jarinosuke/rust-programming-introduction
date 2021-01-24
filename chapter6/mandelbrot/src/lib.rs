405687b04f7b1581d00d936fd3e04cf9d2ab952c
jarinosuke(Naoki ISHIKAWA) <naokiishikawa0808@gmail.com>
2021-01-25 08:56:12 +0900

    create wasm-pack (suspend)

--- chapter6/mandelbrot/src/lib.rs
+++ chapter6/mandelbrot/src/lib.rs
@@ -1,6 +1,8 @@
 mod utils;
-
+mod logic;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::{Clamped, JsCast};
+use web_sys::ImageData;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +11,25 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(a: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, mandelbrot!");
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+macro_rules! measure_elapsed_time {
+    ($t::tt,$s:block) => {{
+        let window = web_sys::window().expect("should have a window in this context");
+        let performance = window
+        .performance()
+        .expect("performance should be available");
+        let start = performance.now();
+        let result = { $s };
+        let end = performance.now();
+        console_log!("{}:{}[ms]", $t, end - start);
+        result
+    }}
 }
